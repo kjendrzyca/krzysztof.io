2cecda8fe34dd3b2fdd725fe1c80b978fbe2cb6d
kjendrzyca <kjendrzyca@users.noreply.github.com>
2022-03-31 16:01:25 +0200

    Add workshop pages for hooks and zen of react

---
+++ src/pages/szkolenie-hooki.js
@@ -0,0 +1,68 @@
+import React from "react"
+import { graphql } from "gatsby"
+import Markdown from 'markdown-to-jsx'
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const md = `
+# Czysty React na Hookach
+
+Jednym z ostatnich dodatków do Reacta są Hooki - koncept pozwalający na jeszcze lepsze uporządkowanie kodu.
+
+Zgodnie z zapewnieniami twórców Reacta jest to przyszłość całego ekosystemu.
+
+Na tym szkoleniu przekażę Ci swoje produkcyjne doświadczenie związane z Hookami. Pokażę, że Hooki nie są lekiem na wszystkie bolączki i mają swoje wady, ale pozwalają robić rzeczy, które wcześniej były niemożliwe.
+
+Zobaczysz jak wyciągnąć z nich to, co najlepsze.
+
+Korzyści:
+
+- Zrozumiesz filozofię i ogólną zasadę działania Hooków.
+- Użyjesz podstawowych Hooków do rozwiązywania typowych problemów, które występują podczas pisania aplikacji w React.js.
+- Zmigrujesz dotychczasowy kod, który korzysta z klas na kod, który korzysta z Hooków.
+- Napiszesz własne, reużywalne Hooki, które enkapsulują logikę komponentów.
+- Dowiesz się, dlaczego pisanie własnych Hooków to największa zaleta tego ficzera.
+- Napiszesz testy do mniej i bardziej skomplikowanych Hooków.
+- Poznasz uniwersalne znaczenie zasad SOLID, które pasuje do aplikacji pisanych w React.js.
+- Napiszesz czysty kod przy zastosowaniu podstawowych Hooków i tych napisanych samemu.
+- Będziesz wiedział, dlaczego warto używać Hooków, a kiedy zostać przy tradycyjnym/klasycznym podejściu.
+- Poznasz największe zagrożenia płynące ze stosowania Hooków.
+- Zaaplikujesz Hooki z popularnych bibliotek takich jak Redux czy React Router.
+
+Wymagania:
+
+- "Projektowa" znajomość React.js - musisz mieć doświadczenie w pisaniu aplikacji przy użyciu standardowych technik obecnych przed wprowadzeniem Hooków.
+- Znajomość patternów HoC i Render Prop.
+- Umiejętność pisania prostych testów jednostkowych.
+- Własny komputer.
+- Zainstalowana najnowsza wersja Node.js.
+- Działająca, pusta aplikacja, wygenerowana przy pomocy create-react-app.
+- Umiejętność pracy z gitem + zainstalowany git .
+`
+
+const SzkolenieTestyPage = ({ data, location }) => {
+  const siteTitle = data.site.siteMetadata?.title
+
+  return (
+    <Layout location={location} title={siteTitle} isLandingPage>
+      <SEO
+        title="Czysty React na Hookach"
+        description="Szkolenie, na którym pokażę, jak wyciągnąć z Hooków to, co najlepsze."
+      />
+      <Markdown children={md} />
+    </Layout>
+  )
+}
+
+export default SzkolenieTestyPage
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
---
+++ src/pages/szkolenie-react-zen.js
@@ -0,0 +1,44 @@
+import React from "react"
+import { graphql } from "gatsby"
+import Markdown from 'markdown-to-jsx'
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const md = `
+# The Zen of React - kilka dobrych zasad, dzięki którym utrzymasz Reacta w czystości
+
+React jest "czysty by design". Zasady, którymi warto się posługiwać, żeby utrzymać kod w czystości, mamy często podane na tacy, czy to w dokumentacji, czy na blogach jasnych gwiazd community.
+
+Zawsze zastanawiałem się, czy inne, znane od dawna dobre praktyki tworzenia oprogramowania, można trochę ponaginać i zaaplikować do Reacta w taki sposób, żeby jeszcze jaśniej nakreślić, jak utrzymywać kod aplikacji w czystości.
+
+Rozmowy z najlepszymi programistami, jakich znam i doświadczenie zdobyte podczas prowadzenia warsztatów oraz pracy z Reactem przekułem w zestaw dyrektyw, które roboczo nazwałem The Zen of React.
+
+Na szkoleniu pokażę Ci, jak możesz zaaplikować je w swoich projektach i jak połączyć je z filozofią Reacta. W efekcie pomogą Ci w utrzymaniu kodu w czystości, ułatwią testowanie i sprawią, że Twój kod będzie łatwiejszy w utrzymaniu.
+`
+
+const SzkolenieReactZenPage = ({ data, location }) => {
+  const siteTitle = data.site.siteMetadata?.title
+
+  return (
+    <Layout location={location} title={siteTitle} isLandingPage>
+      <SEO
+        title="The Zen of React"
+        description="Kilka dobrych zasad, dzięki którym utrzymasz Reacta w czystości."
+      />
+      <Markdown children={md} />
+    </Layout>
+  )
+}
+
+export default SzkolenieReactZenPage
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
