c89d149b63e46dc0314739b9ce75f91d67fa6590
kjendrzyca <kjendrzyca@users.noreply.github.com>
2024-06-07 14:50:25 +0200

    Fix config

--- src/config.js
+++ src/config.js
@@ -5,7 +5,7 @@ export const config = {
       name: `Krzysztof Jendrzyca`,
       summary: ``,
     },
-    description: `Artykuły na temat tworzenia treści i notatek, tworzenia produktów, produktywności, pracy w IT, technologii webowych (React, Node) oraz tego jak być sobą i robić to dobrze.`,
+    description: `Artykuły na temat tworzenia treści i notatek, tworzenia produktów, produktywności, pracy w IT, technologii webowych (React, Node) oraz tego jak być sobą i robić to dobrze.`,
     siteUrl: `https://krzysztof.io`,
     social: {
       social: `kjendrzyca`,
