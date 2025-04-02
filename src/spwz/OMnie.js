a447bee4cc39f26c3220515e3aa3c0c10a23b6d3
kjendrzyca <kjendrzyca@users.noreply.github.com>
2025-04-02 23:46:13 +0200

    Update OMnie

--- src/spwz/OMnie.js
+++ src/spwz/OMnie.js
@@ -3,18 +3,17 @@ import ReactMarkdown from "react-markdown"
 import NextImage from 'next/image'
 
 const kilkaSlowOMnieMd = `
-Pracowałem w różnych projektach - od 10-letniego legacy kodu, po szybkie dwumiesięczne startupowe projekty. W software housach, w firmach produktowych i jako freelancer.
+Pracowałem w różnych projektach - od startupów po wieloletnie legacy.
 
-Pełniłem funkcję szeregowego programisty, lidera technicznego, architekta i doradcy technicznego, który pomaga rozwijać wiele różnych projektów jednocześnie.
+Byłem programistą, liderem technicznym, architektem i doradcą technicznym.
 
-Współtworzyłem zespoły od zera, przechodząc przez wszystkie fazy jego formowania.
 Pomagałem przekształcać dysfunkcyjne zespoły w takie, które wydajnie rozwiązują wszystkie napotkane problemy.
 
-Poznałem większość antypatternów w kodzie i chyba wszystkie w zachowaniach zespołowych. **Po latach pracy wiem, jak sobie z nimi radzić**.
+Widziałem większość antypatternów w kodzie i zespołach - i wiem, jak sobie z nimi radzić.
 
 Swoje doświadczenia weryfikuję przez dzielenie się nimi na konferencjach, warsztatach, w pracy na etacie oraz online.
 
-Rolę bullshit detectora pełni też bogata siatka znajomych - doświadczeni programiści, doradcy techniczni, analitycy biznesowi, scrum masterzy,
+Rolę bullshit detectora pełni też bogata siatka znajomych - doświadczeni programiści, analitycy biznesowi, scrum masterzy,
 product ownerzy, CTO, oraz założyciele firm zajmujących się tworzeniem oprogramowania.
 `
 
