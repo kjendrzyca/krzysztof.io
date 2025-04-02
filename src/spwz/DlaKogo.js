5235aa0c51de58ebf970c2dce3f81fab4060fcf9
kjendrzyca <kjendrzyca@users.noreply.github.com>
2025-04-02 23:41:18 +0200

    Update DlaKogo

--- src/spwz/DlaKogo.js
+++ src/spwz/DlaKogo.js
@@ -2,20 +2,13 @@ import { CenteredH2 } from "@/components/CenteredH2"
 import ReactMarkdown from "react-markdown"
 
 const dlaKogoMd = `
-"Skuteczna Praca w Zespole" to program dla programistów i programistek na poziomie junior oraz regular,
-którzy chcą poznać sposoby pracy oraz umiejętności miękkie dobrze opłacanych i wyróżniających się senior developerów,
-które pozwolą im w przyszłości bez problemu wypełniać role liderskie na najwyższym poziomie (jeśli będą tego chcieli).
+**Dla juniorów i regularów**, którzy chcą przejąć kontrolę nad swoją karierą i zacząć działać jak **ogarnięci seniorzy**.
 
-Dla senior developerów, którzy nie mieli wzorów do naśladowania i czują braki w swoich umiejętnościach nietechnicznych, mimo że ich umiejętności techniczne są na wysokim poziomie.
+**Dla seniorów**, którzy czują **braki w umiejętnościach nietechnicznych**.
 
-Ogólnie, program jest dla wszystkich, którzy:
+Dla tych, którzy chcą wziąć **odpowiedzialność za cały proces tworzenia oprogramowania**, rozwijać się zawodowo i wpływać na **jakość projektów**.
 
-- chcą wziąć odpowiedzialność za **wszystkie aspekty tworzenia oprogramowania**, a nie tylko za klikanie w klawiaturę
-- wierzą w to, że nie samym kodem człowiek żyje i chcą poprawić swoje warunki zatrudnienia, rozwijając też inne umiejętności
-- chcą lepiej wykonywać swoją pracę jako członkowie zespołu
-- chcą szybciej rozwiązywać problemy z pracą zespołową, żeby nie odciągały ich od kodzenia
-
-Materiały są pisane z myślą o ludziach, którzy programują, bo sam to robię, ale sprawdzają się też u innych, niezależne od roli.
+Dla tych, którzy **chcą działać sprawniej**, żeby nic nie odciągało ich od kodu.
 `
 
 export const DlaKogo = () => {
