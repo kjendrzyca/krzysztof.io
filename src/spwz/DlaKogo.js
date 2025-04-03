fa9cba049a5a8918e2beb454052971da8b2be043
kjendrzyca <kjendrzyca@users.noreply.github.com>
2025-04-03 17:46:41 +0200

    Update DlaKogo

--- src/spwz/DlaKogo.js
+++ src/spwz/DlaKogo.js
@@ -4,11 +4,9 @@ import ReactMarkdown from "react-markdown"
 const dlaKogoMd = `
 **Dla juniorów i regularów**, którzy chcą przejąć kontrolę nad swoją karierą i zacząć działać jak **ogarnięci seniorzy**.
 
-**Dla seniorów**, którzy czują **braki w umiejętnościach nietechnicznych**.
+**Dla seniorów**, którzy czują **braki w umiejętnościach nietechnicznych** i chcą je poprawić, żeby nie było przypału.
 
-Dla tych, którzy chcą wziąć **odpowiedzialność za cały proces tworzenia oprogramowania**, rozwijać się zawodowo i wpływać na **jakość projektów**.
-
-Dla tych, którzy **chcą działać sprawniej**, żeby nic nie odciągało ich od kodu.
+Dla tych, którzy chcą wziąć **odpowiedzialność za cały proces tworzenia oprogramowania**, zostać **kluczowymi członkami zespołu** i mieć wpływ na to jak realizowane są projekty.
 `
 
 export const DlaKogo = () => {
