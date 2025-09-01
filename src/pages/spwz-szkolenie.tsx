a16d3feb140368b7a66ca416ab7c89f8b6ab2d6e
kjendrzyca <kjendrzyca@users.noreply.github.com>
2025-09-01 12:10:00 +0200

    Pixel

--- src/pages/spwz-szkolenie.tsx
+++ src/pages/spwz-szkolenie.tsx
@@ -1,6 +1,8 @@
+/* eslint-disable @next/next/no-img-element */
 import React from 'react'
 import ReactMarkdown from 'react-markdown'
 import NextImage from 'next/image'
+import Head from 'next/head'
 
 import { Layout } from '@/components/layout'
 import SEO from '@/components/seo'
@@ -20,6 +22,33 @@ const SPWZSzkoleniePage = () => {
         ogType="page"
         ogImagePath="/spwz/spwz-szkolenie-2-og.png"
       />
+
+      <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1109547327810831');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=1109547327810831&ev=PageView&noscript=1"
+          />
+        </noscript>
+      </Head>
 
       <h3
         style={{
